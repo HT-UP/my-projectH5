59025f002b41f0a6b70734c5536381e45542f88a
何涛 <het@vvtech.io>
2021-07-20 11:08:08 +0800

    再次提交

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
-	<div id="app">
-		<router-view></router-view>
-	</div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-	}
-	/*滚动条的宽度*/
-	.el-table__body-wrapper::-webkit-scrollbar {
-		width: 8px;
-		height: 8px;
-	}
-	/*滚动条的滑块*/
-	.el-table__body-wrapper::-webkit-scrollbar-thumb {
-		background-color: #999;
-		border-radius: 3px;
-	}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import Router from 'vue-router'
-import store from './store'
-import { request } from './request/app'
-import md5 from 'js-md5'
-import qs from 'qs'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.prototype.$getAjax = request.getAjax;
-Vue.prototype.$postAjax = request.postAjax;
-
-Vue.config.productionTip = false
-
-const originalPush = Router.prototype.push
-Router.prototype.push = function push(location) {
-	return originalPush.call(this, location).catch(err => err)
-}
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
