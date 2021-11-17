3a548efa692f538cae1616a2ab6e659de8ac739e
何涛 <het@vvtech.io>
2021-11-17 16:43:27 +0800

    接入多个开发接口配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+/*本地多端口代理设置:
+base： /api 替换标志
+port： 端口号
+acceptIps： 允许代理的白名单，只有存在的白名单才被与允许多端口代理*/
+const base = "";
+const port = "8050";
+const acceptIps = ["192.168.1.219", "192.168.1.143", "192.168.1.29"];
+
 module.exports = {
 	outputDir: 'dist',//打包后的目录名称
 	indexPath: 'index.html',//html 的输出路径
@@ -5,7 +13,7 @@ module.exports = {
     lintOnSave: false,
     filenameHashing: false,
 	devServer: {
-		port: '8090', // 端口号
+		port: '8099', // 端口号
 		https: false, // https: {type:Bollean}
 		open: false, // 配置自动启动浏览器
 		//显示警告和错误
@@ -18,8 +26,19 @@ module.exports = {
 			"/api": {
                 target: 'https://chinanet.vvtech.tech/web/admin',  //测试
 				changeOrigin: true, // 开启跨域,在本地创建一个虚拟服务,然后发送请求的数据,并同时接收请求的数据,这样服务端和服务端进行数据交互就不会有问题
-				pathRewrite: {
-					"^/api": '', // 利用这个里面的值拼接上target里面的地址
+				pathRewrite(path, req) {
+					const ip = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0];
+					if(acceptIps.indexOf(ip) >= 0) {
+						return path.replace('/api', base);
+					}
+					return path.replace('/api', '');
+				},
+				router(req) {
+					const ip = req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0]; // 多端口代理， 哪个客户端请求， 转发到那个客户端服务， 针对白名单用户， 非白名单用户走正常代理流程
+					if(acceptIps.indexOf(ip) >= 0) {
+						return `${req.protocol}://${req.ip.match(/\d+\.\d+\.\d+\.\d+/)[0]}:${port}`;
+					}
+					return
 				}
 			}
 		}
